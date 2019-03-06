eb18a99e19e2724cc08ee9a2eee56e45927688ce
samucaof@hotmail.com <samucaof@hotmail.com>
2019-03-06 19:52:09 -0300

    [server.js] Added try catch

--- server.js
+++ server.js
@@ -15,48 +15,52 @@ server.route({
 });
 
 const init = async () => {
-    await server.register(require('vision'));
-    await server.register(require('inert'));
+    try {
+        await server.register(require('vision'));
+        await server.register(require('inert'));
 
-    server.route({
-        method: 'GET',
-        path: '/js/{file*}',
-        handler: {
-            directory: {
-                path: 'public/js',
-                listing: true
+        server.route({
+            method: 'GET',
+            path: '/js/{file*}',
+            handler: {
+                directory: {
+                    path: 'public/js',
+                    listing: true
+                }
             }
-        }
-    });
+        });
 
-    server.route({
-        method: 'GET',
-        path: '/css/{file*}',
-        handler: {
-            directory: {
-                path: 'public/css',
-                listing: true
+        server.route({
+            method: 'GET',
+            path: '/css/{file*}',
+            handler: {
+                directory: {
+                    path: 'public/css',
+                    listing: true
+                }
             }
-        }
-    });
+        });
 
-    server.route({
-        method: 'GET',
-        path: '/{path*}',
-        handler: (_, h) => h.view('404')
-    });
+        server.route({
+            method: 'GET',
+            path: '/{path*}',
+            handler: (_, h) => h.view('404')
+        });
 
-    server.views({
-        engines: {
-            pug: Pug
-        },
-        relativeTo: __dirname,
-        path: 'templates',
-        helpersPath: 'helpers'
-    })
+        server.views({
+            engines: {
+                pug: Pug
+            },
+            relativeTo: __dirname,
+            path: 'templates',
+            helpersPath: 'helpers'
+        })
 
-    console.log(`Server running at: ${server.info.uri}`);
-    server.start()
+        console.log(`Server running at: ${server.info.uri}`);
+        server.start()
+    } catch(err) {
+        console.error('[ERROR]', err);
+    }
 }
 
 init();
